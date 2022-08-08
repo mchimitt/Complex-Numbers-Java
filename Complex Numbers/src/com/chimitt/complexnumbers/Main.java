8e962699a3add8c2565db2d0c366d90734ecd562
Matthew Chimitt <mmchimitt@gmail.com>
2022-08-07 21:00:48 -0500

    Updating Main

--- Complex Numbers/src/com/chimitt/complexnumbers/Main.java
+++ Complex Numbers/src/com/chimitt/complexnumbers/Main.java
@@ -6,16 +6,44 @@ public class Main {
 
 	
 	public static void main(String [] args) {
-		Complex a = new Complex(5, 6);
-		Complex b = new Complex(-3, 4);
-		Complex c = a.mult(b);
-		System.out.println(c.toString());
-		c = a.add(b);
-		System.out.println(c.toString());
+		Complex a = new Complex(2, 3);
+		Complex b = new Complex(4, -5);
+		
+		System.out.println("Complex Number 1: " + a.toString());
+		System.out.println("Complex Number 2: " + b.toString());
+		
+		
+		
+		Complex c = a.add(b);
+		System.out.println("Addition: " + c.toString());
+		
 		c = a.sub(b);
-		System.out.println(c.toString());
+		System.out.println("Subtraction: " + c.toString());
+		
+		c = a.mult(b);
+		System.out.println("Multiplication: " + c.toString());
+		
 		c = a.div(b);
-		System.out.println(c.toString());
+		System.out.println("Division: " + c.toString());
+	
+		c = a.conj();
+		System.out.println("Conjugate of Complex Number 1: " + c.toString());
+		
+		c = b.conj();
+		System.out.println("Conjugate of Complex Number 2: " + c.toString());
+		
+		c = a.reciprocal();
+		System.out.println("Reciprocal of Complex Number 1: " + c.toString());
+		
+		c = b.reciprocal();
+		System.out.println("Reciprocal of Complex Number 2: " + c.toString());
+		
+		double e = a.abs();
+		System.out.println("Absolute Value of Complex Number 1: " + e);
+		
+		e = b.abs();
+		System.out.println("Absolute Value of Complex Number 2: " + e);
+		
 	}
 	
 }
